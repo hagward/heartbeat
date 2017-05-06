a6a855c88aeeaeda66ec4d8da780f84acea74cd9
Anders Hagward <taperlawyer@gmail.com>
2017-05-06 09:52:09 +0200

    Add token generator

--- app.js
+++ app.js
@@ -5,41 +5,62 @@ const app = express()
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 })
 
+const clients = {}
+const tokens = {}
+
 app.use('/node_modules', express.static('node_modules'))
 
-app.get('/send/:clientId', (req, res) => {
-    res.sendFile('send.html', { root: __dirname + '/public/' })
+app.get('/new', (req, res) => {
+    const token = generateToken()
+
+    tokens[token] = true
+
+    res.send(token)
 })
 
-app.get('/:clientId', (req, res) => {
-    res.sendFile('index.html', { root: __dirname + '/public/' })
+function generateToken() {
+    return Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2)
+}
+
+app.get('/send/:token', (req, res) => {
+    const token = req.params.token
+    if (tokens[token]) {
+        res.sendFile('send.html', { root: __dirname + '/public/' })
+    } else {
+        res.send('Invalid token: ' + token)
+    }
 })
 
-let connectionId = 0
-const clients = {}
+app.get('/:token', (req, res) => {
+    const token = req.params.token
+    if (tokens[token]) {
+        res.sendFile('index.html', { root: __dirname + '/public/' })
+    } else {
+        res.send('Invalid token: ' + token)
+    }
+})
 
 wss.on('connection', ws => {
     const url = ws.upgradeReq.url
 
     if (url.startsWith('/sender')) {
-        const clientId = url.substring(8)
+        const token = url.substring(8)
 
-        console.log('sender connected with id "%s"', clientId)
+        console.log('sender connected with token: %s', token)
 
         ws.on('message', message => {
-            const client = clients[clientId]
+            const client = clients[token]
             if (client) {
-                console.log('sending message: "%s"', message)
                 client.send(message)
             }
         })
     } else if (url.startsWith('/receiver')) {
-        const clientId = url.substring(10)
+        const token = url.substring(10)
 
-        console.log('receiver connected with id "%s"', clientId)
+        console.log('receiver connected with token: %s', token)
 
-        if (clientId) {
-            clients[clientId] = ws
+        if (tokens[token]) {
+            clients[token] = ws
         }
     }
 })
