f08c865bf2c5b47c4130fc9642631d31ca90ae12
Anders Hagward <taperlawyer@gmail.com>
2017-05-11 23:51:22 +0200

    Add WebPack for dev/prod builds

---
+++ index.js
@@ -0,0 +1,23 @@
+var webSocketAddress = process.env.NODE_ENV === 'production'
+        ? 'wss://heartbeat.deg17.net/'
+        : 'ws://localhost:8080/';
+var token = window.location.pathname;
+var ws = new WebSocket(webSocketAddress + token);
+
+var bpmSpan = document.getElementById('bpm');
+
+ws.onmessage = function (event) {
+    bpmSpan.innerText = event.data;
+};
+
+var colorInputBg = document.getElementById('colorBg');
+colorInputBg.value = '#333333';
+colorInputBg.addEventListener('input', function (event) {
+    document.body.style.background = event.target.value;
+});
+
+var colorInputFg = document.getElementById('colorFg');
+colorInputFg.value = '#EFFEFF';
+colorInputFg.addEventListener('input', function (event) {
+    bpmSpan.style.color = event.target.value;
+});
