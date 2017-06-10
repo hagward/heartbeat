b59bef5708e7be29182404a19f43b15d768c3885
Anders Hagward <taperlawyer@gmail.com>
2017-06-10 23:42:28 +0200

    Dynamically update heart animation duration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var socket = require('socket.io-client')();
 
 var room = window.location.pathname.substring(1);
+var heartSvg = document.getElementById('heart');
 var bpmSpan = document.getElementById('bpm');
 
 socket.on('connect', function () {
@@ -8,6 +9,7 @@ socket.on('connect', function () {
 
     socket.on('message', function (data) {
         bpmSpan.innerText = data;
+        heartSvg.style.animationDuration = (60/Math.round(data)) + 's';
     });
 });
 
